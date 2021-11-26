14771526fb2e96d966210fbe6f4158606184f40b
Maurizio <proiettomaurizio97@gmail.com>
2021-11-26 11:58:41 +0100

    create file

---
+++ src/components/ListPersonages.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="container">
+    <h1>Richy and Morty APP</h1>
+    <div id="container-item">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListPersonages",
+  data() {
+    return {
+        apiUrl: "",
+        listPersonages: [],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    #container {
+        margin: 20px auto;
+        width: 70%;
+    }
+</style>
